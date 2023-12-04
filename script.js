f52b8c7c19b7b85b0aa4c65e16e93b0b57da8654
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-12-04 21:45:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  let cost = 0;
+	while(arr.length > 1){
+		arr.sort((a,b)=>{
+			return a - b;
+		});
+		let a = arr.shift();
+		let b = arr.shift();
+		cost = cost + a + b;
+		arr.push(a+b);
+	}
+	return cost;
 }
 
 module.exports=mincost;
